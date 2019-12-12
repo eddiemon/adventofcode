25b7cc3f80d09cfc3f5e6e0afdf3d5e8e02e44a5
Eddie Sundvall <eddie.sundvall@scania.com>
2019-12-12 09:36:22 +0100

    Finished d12

--- D12.cs
+++ D12.cs
@@ -13,58 +13,64 @@ namespace aoc2019
     public class D12
     {
         
-        Planet[] planets = File.ReadAllLines("d12.1.txt").Select(l => Planet.FromString(l)).ToArray();
+        Moon[] moons = File.ReadAllLines("d12.txt").Select(l => Moon.FromString(l)).ToArray();
 
         public string Answer()
         {
-            const int numSteps = 10;
+            const int numSteps = 1000;
             for (int i = 0; i < numSteps; i++)
             {
                 ApplyGravity();
                 UpdatePositions();
             }
 
+            int totalEnergy = 0;
+            foreach (var moon in moons)
+            {
+                totalEnergy += (Math.Abs(moon.pos.x) + Math.Abs(moon.pos.y) + Math.Abs(moon.pos.z)) * 
+                (Math.Abs(moon.dir.x) + Math.Abs(moon.dir.y) + Math.Abs(moon.dir.z));
+            }
 
-            return string.Empty;
+            return totalEnergy.ToString();
         }
 
         private void ApplyGravity()
         {
-            for(int p = 0; p < planets.Length - 1; p++) {
-                for (int pp = p + 1; pp < planets.Length; pp++) {
-                    var planetA = planets[p];
-                    var planetB = planets[pp];
-                    planetA.dir.x += planetA.pos.x < planetB.pos.x ? 1 : -1;
-                    planetB.dir.x += planetA.pos.x < planetB.pos.x ? -1 : 1;
-                    planetA.dir.y += planetA.pos.y < planetB.pos.y ? 1 : -1;
-                    planetB.dir.y += planetA.pos.y < planetB.pos.y ? -1 : 1;
-                    planetA.dir.z += planetA.pos.z < planetB.pos.z ? 1 : -1;
-                    planetB.dir.z += planetA.pos.z < planetB.pos.z ? -1 : 1;
+            for(int p = 0; p < moons.Length - 1; p++) {
+                for (int pp = p + 1; pp < moons.Length; pp++) {
+                    var moonA = moons[p]; 
+                    var moonB = moons[pp];
+                    moonA.dir.x += moonA.pos.x == moonB.pos.x ? 0 : moonA.pos.x < moonB.pos.x ? 1 : -1;
+                    moonB.dir.x += moonA.pos.x == moonB.pos.x ? 0 : moonA.pos.x < moonB.pos.x ? -1 : 1;
+                    moonA.dir.y += moonA.pos.y == moonB.pos.y ? 0 : moonA.pos.y < moonB.pos.y ? 1 : -1;
+                    moonB.dir.y += moonA.pos.y == moonB.pos.y ? 0 : moonA.pos.y < moonB.pos.y ? -1 : 1;
+                    moonA.dir.z += moonA.pos.z == moonB.pos.z ? 0 : moonA.pos.z < moonB.pos.z ? 1 : -1;
+                    moonB.dir.z += moonA.pos.z == moonB.pos.z ? 0 : moonA.pos.z < moonB.pos.z ? -1 : 1;
                 }
             }
         }
 
         private void UpdatePositions()
         {
-            for(int p = 0; p < planets.Length; p++) {
-                var planet = planets[p];
+            for(int p = 0; p < moons.Length; p++) {
+                var planet = moons[p];
                 planet.pos.x += planet.dir.x;
                 planet.pos.y += planet.dir.y;
                 planet.pos.z += planet.dir.z;
             }
         }
 
-        public class Planet {
+        public class Moon {
             public Vector3 pos;
             public Vector3 dir = new Vector3();
-            public static Planet FromString(string s) {
+            public static Moon FromString(string s) {
                 var reg = new Regex(@"\<x=(\-?\d*),\sy=(\-?\d*),\sz=(\-?\d*)");
                 var matches = reg.Match(s);
                 var x = int.Parse(matches.Groups[1].ToString());
                 var y = int.Parse(matches.Groups[2].ToString());
                 var z = int.Parse(matches.Groups[3].ToString());
 
-                return new Planet() { pos = new Vector3(x, y, z)};
+                return new Moon() { pos = new Vector3(x, y, z)};
             }
 
             public override string ToString() => $"pos: {pos.ToString()}, dir: {dir.ToString()}";
