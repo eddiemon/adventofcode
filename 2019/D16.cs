0898373cd298914a1c4816cd5d26b5b343c6e0d4
Eddie Sundvall <eddie.sundvall@gmail.com>
2019-12-20 20:20:50 +0100

    Add 2019 day 19 part 1 & 2

--- 2019/D16.cs
+++ 2019/D16.cs
@@ -13,22 +13,36 @@ namespace aoc
 
         public object Answer()
         {
-            var numbers = Enumerable.Repeat(File.ReadAllText("16.2").Select(c => int.Parse(c.ToString())), 10000).SelectMany(x => x).ToArray().AsSpan();
+            var originalCode = File.ReadAllText("16.in").Select(c => int.Parse(c.ToString())).ToArray();
 
-            unchecked
-            {
-                for (int p = 0; p < phases; p++)
-                {
-                    Console.WriteLine($"Applying phase {p + 1}");
-                    ApplyPhase(numbers);
-                }
-            }
             var sb = new StringBuilder();
-            foreach (var n in numbers.Slice(0, 8))
+            foreach (var n in originalCode.AsSpan().Slice(0, 7))
                 sb.Append(n.ToString());
-            var offset = int.Parse(sb.ToString());
+
+            var wat = int.Parse(sb.ToString());
+            var c = Enumerable.Repeat(originalCode, 10000).SelectMany(x => x).ToArray();
+            var code = c.AsSpan().Slice(wat);
+            var state = code.Slice(0);
+            var nextState = new int[code.Length];
+
+            for (int p = 0; p < phases; p++)
+            {
+                var count = 0;
+                for (int location = state.Length - 1; location >= 0; location--)
+                {
+                    count = (count + state[location]) % 10;
+                    nextState[location] = count;
+                }
+                for (int location = 0; location < state.Length; location++)
+                {
+                    state[location] = nextState[location];
+                }
+                //    Console.WriteLine($"Applying phase {p + 1}");
+                //    ApplyPhase(originalCode);
+            }
+
             sb.Clear();
-            foreach (var n in numbers.Slice(offset, 8))
+            foreach (var n in state.Slice(0, 8))
                 sb.Append(n.ToString());
             return sb.ToString();
         }
@@ -46,34 +60,37 @@ namespace aoc
                 var repetitions = i + 1;
                 while (processed < length)
                 {
-                    Span<int> slice;
-                    if (processed + repetitions < length)
-                        slice = numbers.Slice(processed, repetitions);
-                    else if (processed >= length) break;
-                    else
-                        slice = numbers.Slice(processed, length - processed);
-
-                    // add
-                    int l = slice.Length;
-                    for (int j = 0; j < l; j++)
+                    unchecked
                     {
-                        sum += slice[j];
-                    }
-                    processed += l + repetitions;
+                        Span<int> slice;
+                        if (processed + repetitions < length)
+                            slice = numbers.Slice(processed, repetitions);
+                        else if (processed >= length) break;
+                        else
+                            slice = numbers.Slice(processed, length - processed);
 
-                    if (processed + repetitions < length)
-                        slice = numbers.Slice(processed, repetitions);
-                    else if (processed >= length) break;
-                    else
-                        slice = numbers.Slice(processed, length - processed);
+                        // add
+                        int l = slice.Length;
+                        for (int j = 0; j < l; j++)
+                        {
+                            sum += slice[j];
+                        }
+                        processed += l + repetitions;
 
-                    // subtract
-                    l = slice.Length;
-                    for (int j = 0; j < l; j++)
-                    {
-                        sum -= slice[j];
+                        if (processed + repetitions < length)
+                            slice = numbers.Slice(processed, repetitions);
+                        else if (processed >= length) break;
+                        else
+                            slice = numbers.Slice(processed, length - processed);
+
+                        // subtract
+                        l = slice.Length;
+                        for (int j = 0; j < l; j++)
+                        {
+                            sum -= slice[j];
+                        }
+                        processed += l + repetitions;
                     }
-                    processed += l + repetitions;
                 }
 
                 if (i % 10000 == 0) Console.WriteLine(i);
---
+++ 2019/D19.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+namespace aoc
+{
+    internal class D19
+    {
+        BigInteger val(int x, int y)
+        {
+            var computer = new IntCodeSync(code.ToArray());
+            computer.Run(x);
+            computer.Run(y);
+            return computer.Run().Value;
+        }
+
+        //static char[,] g;
+        //static D19()
+        //{
+        //    var lines = File.ReadAllLines("19.1");
+        //    g = new char[lines.Length, lines[0].Length];
+        //    for (int r = 0; r < lines.Length; r++)
+        //    {
+        //        for (int c = 0; c < lines[0].Length; c++)
+        //        {
+        //            g[r, c] = lines[r][c];
+        //        }
+        //    }
+        //}
+
+        //BigInteger val(int x, int y)
+        //{
+        //    if (x < 0 || x >= g.GetLength(1) || y < 0 || y >= g.GetLength(0)) return 0;
+
+        //    return g[y, x] == '#' ? 1 : 0;
+        //}
+
+        BigInteger[] code;
+
+        internal object Answer()
+        {
+            code = File.ReadAllText("19.in").Split(',').Select(s => BigInteger.Parse(s)).ToArray();
+
+            const int size = 100;
+            int x = 0;
+            for (int y = 1000; ; y++)
+            {
+                for (;; x++)
+                {
+                    if (val(x, y) == 1)
+                    {
+                        if (val(x + size - 1, y) == 1 && val(x + size - 1, y - size + 1) == 1)
+                        {
+                            Console.WriteLine($"({x}, {y - size + 1})");
+                            Console.WriteLine($"({x + size - 1}, {y - size + 1})");
+                            Console.WriteLine($"({x}, {y})");
+                            return x * 10000 + (y - size + 1);
+                        }
+                        else
+                        {
+                            x -= 5;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
