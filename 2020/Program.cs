1c65d25e3557566b738c0456194f77c94dced32d
Eddie Sundvall <eddie.sundvall@gmail.com>
2020-12-03 07:13:35 +0100

    Add 2020 Day 3

--- 2020/Program.cs
+++ 2020/Program.cs
@@ -2,44 +2,29 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text.RegularExpressions;
 
-var r = new Regex(@"(?<min>\d+)-(?<max>\d+) (?<l>.): (?<pw>.+)");
-var input = File.ReadAllLines("../../../2.in").Select(l =>
-{
-    var m = r.Match(l);
-    return new Line(int.Parse(m.Groups["min"].Value), int.Parse(m.Groups["max"].Value), m.Groups["l"].Value, m.Groups["pw"].Value);
-}).ToList();
+var input = File.ReadAllLines("../../../3.in").ToList();
+var treesPerSlope = new List<BigInteger>(5);
 
-var validPw = new List<string>();
-// part 1
-//foreach (var line in input)
-//{
-//    var charCount = 0;
-//    for (int i = line.min; i < line.pw.Length; i++)
-//    {
-//        if (line.pw[i] == line.l[0])
-//            charCount++;
-//    }
-//    if (charCount >= line.min && charCount <= line.max)
-//        validPw.Add(line.pw);
-//}
+treesPerSlope.Add(CountTreesForSlope(1, 1));
+treesPerSlope.Add(CountTreesForSlope(3, 1));
+treesPerSlope.Add(CountTreesForSlope(5, 1));
+treesPerSlope.Add(CountTreesForSlope(7, 1));
+treesPerSlope.Add(CountTreesForSlope(1, 2));
 
-// part 2
-foreach (var line in input)
+Console.WriteLine(treesPerSlope.Aggregate((a, b) => a * b));
+
+int CountTreesForSlope(int dx, int dy)
 {
-    var charCount = 0;
-    if (line.pw[line.min - 1] == line.l[0]) charCount++;
-    if (line.pw[line.max - 1] == line.l[0]) charCount++;
-    //for (int i = line.min; i < line.pw.Length; i++)
-    //{
-    //    if (line.pw[i] == line.l[0])
-    //        charCount++;
-    //}
-    if (charCount == 1)
-        validPw.Add(line.pw);
+    var xx = dx;
+    var trees = 0;
+    for (int y = dy; y < input.Count; y += dy, xx += dx)
+    {
+        var x = xx % 31;
+        if (input[y][x] == '#')
+            trees++;
+    }
+    return trees;
 }
-
-Console.WriteLine(validPw.Count);
-
-record Line(int min, int max, string l, string pw);
