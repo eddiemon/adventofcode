95df8829809db82417fb4b4b1776b748a8000193
Eddie Sundvall <eddie.sundvall@gmail.com>
2020-12-06 08:17:59 +0100

    Add day 6 2020

--- 2020/Program.cs
+++ 2020/Program.cs
@@ -5,46 +5,41 @@ using System.Linq;
 using System.Numerics;
 using System.Text.RegularExpressions;
 
-var input = File.ReadAllLines("../../../5.in");
+var input = File.ReadAllLines("../../../6.in");
 
-var seats = new bool[128 * 8];
-Array.Fill(seats, false);
+var currentlyAnsweredQuestions = new Dictionary<char, int>();
+var numberOfPeopleInGroup = 0;
+var totalAnsweredQuestions = 0;
 
 foreach (var l in input)
 {
-    var yl = 0;
-    var yh = 127;
-    for (int i = 0; i < 7; i++)
+    if (string.IsNullOrEmpty(l))
     {
-        var c = l[i];
-        if (c == 'F')
-            yh -= (yh - yl + 1) / 2;
-        else if (c == 'B')
-            yl += (yh - yl + 1) / 2;
-    }
-
-    var xl = 0;
-    var xh = 7;
-    for (int i = 0; i < 3; i++)
-    {
-        var c = l[7 + i];
-        if (c == 'L')
-            xh -= (xh - xl + 1) / 2;
-        else if (c == 'R')
-            xl += (xh - xl + 1) / 2;
-    }
-
-    seats[yl * 8 + xl] = true;
-}
-
-for (int y = 1; y < 127; y++)
-{
-    for (int x = 1; x < 7; x++)
-    {
-        if (!seats[y * 8 + x] && seats[y * 8 + x - 1] && seats[y * 8 + x + 1])
+        foreach (var (answer, count) in currentlyAnsweredQuestions)
         {
-            Console.Write(y * 8 + x);
-            return;
+            if (count == numberOfPeopleInGroup)
+                totalAnsweredQuestions++;
         }
+
+        currentlyAnsweredQuestions.Clear();
+        numberOfPeopleInGroup = 0;
+        continue;
+    }
+    else
+    {
+        numberOfPeopleInGroup++;
+    }
+
+    for (int i = 0; i < l.Length; i++)
+    {
+        currentlyAnsweredQuestions[l[i]] = currentlyAnsweredQuestions.TryGetValue(l[i], out int count) ? count + 1 : 1;
     }
 }
+
+foreach (var (answer, count) in currentlyAnsweredQuestions)
+{
+    if (count == numberOfPeopleInGroup)
+        totalAnsweredQuestions++;
+}
+
+Console.WriteLine(totalAnsweredQuestions);
