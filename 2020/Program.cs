6318f78e4aa4ea3e59fa36b098adeeaa67537334
Eddie Sundvall <eddie.sundvall@gmail.com>
2020-12-10 07:34:11 +0100

    Add day 10 2020 part 1

--- 2020/Program.cs
+++ 2020/Program.cs
@@ -5,18 +5,18 @@ using System.Linq;
 using System.Numerics;
 using System.Text.RegularExpressions;
 
-var input = File.ReadAllLines("../../../9.in").Select(n => long.Parse(n)).ToArray();
-const long number = 138879426;
-int l = 0, h = 1;
-long s = input[l] + input[h];
+var input = File.ReadAllLines("../../../10.in").Select(n => int.Parse(n)).ToList();
 
-while (s != number)
+input.Sort();
+input.Insert(0, 0);
+var diff1 = 0;
+var diff3 = 0;
+for (int i = 1; i < input.Count; i++)
 {
-    if (s < number)
-        s += input[++h];
-    else if (s > number)
-        s -= input[l++];
+    var diff = input[i] - input[i - 1];
+    if (diff == 1) diff1++;
+    else if (diff == 3) diff3++;
 }
+diff3++;
 
-var ii = input[l..(h + 1)];
-Console.WriteLine(ii.Min() + ii.Max());
+Console.WriteLine(diff1 * diff3);
