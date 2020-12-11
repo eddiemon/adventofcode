2b56e8c30b6df76419964c33299f2b287466cfba
Eddie Sundvall <eddie.sundvall@gmail.com>
2020-12-11 20:48:24 +0100

    Add day 11 part 1

--- 2020/Program.cs
+++ 2020/Program.cs
@@ -5,77 +5,66 @@ using System.Linq;
 using System.Numerics;
 using System.Text.RegularExpressions;
 
-var input = File.ReadAllLines("../../../10.3.in").Select(n => int.Parse(n)).ToList();
+var input = File.ReadAllLines("../../../11.in").Select(l => l.ToCharArray()).ToArray();
 
-input.Sort();
-//input.Insert(0, 0);
-//input.Add(input.Max() + 3);
-
-var inp = input.ToArray().AsSpan();
-
-//int arrangements = 1;
-//for (int i = 1; i < inp.Length - 2; i++)
-//{
-//    if (inp[i + 1] - inp[i] == 1)
-//        arrangements++;
-//}
-
-//Console.WriteLine(arrangements);
-
-//var arrangements = new List<int>();
-//for (int i = 0; i < inp.Length - 1; i++)
-//{
-//    var n = inp[i];
-//    var canConnect = 0;
-//    if (inp.Length > i + 1 && inp[i + 1] <= n + 3) canConnect++;
-//    if (inp.Length > i + 2 && inp[i + 2] <= n + 3) canConnect++;
-//    if (inp.Length > i + 3 && inp[i + 3] <= n + 3) canConnect++;
-//    arrangements.Add(canConnect);
-//}
-
-//bool multiplying = false;
-//int multiplier = 1;
-//long sum = 0;
-//for (int i = 0; i < arrangements.Count; i++)
-//{
-//    if (arrangements[i] > 1)
-//    {
-//        multiplying = true;
-//        multiplier *= arrangements[i];
-//    }
-//    else
-//    {
-//        if (multiplying)
-//            sum += multiplier;
-//        multiplying = false;
-//        multiplier = 1;
-//    }
-//}
-//Console.WriteLine(sum);
-//var numOfArrangements = CountArrangements(inp[0], inp[1..]);
-//Console.WriteLine(numOfArrangements);
-
-Console.WriteLine(CountArrangements(10, new[] { 11, 12, 13, 15 }));
-
-int CountArrangements(int n, Span<int> numbers)
+var changes = new List<Action>();
+int rounds = -1;
+do
 {
-    if (numbers.Length <= 1)
-        return 1;
+    rounds++;
+    changes = new List<Action>();
 
-    int numsInRange = 0;
-    for (int i = 0; i < 3 && i < numbers.Length; i++)
+    for (int y = 0; y < input.Length; y++)
     {
-        if (numbers[i] - n <= 3)
-            numsInRange++;
+        for (int x = 0; x < input[0].Length; x++)
+        {
+            if (input[y][x] == '.')
+                continue;
+
+            int occupiedSeats = 0;
+            for (int dy = -1; dy < 2; dy++)
+            {
+                for (int dx = -1; dx < 2; dx++)
+                {
+                    var yy = y + dy;
+                    var xx = x + dx;
+                    if (yy < 0 || yy >= input.Length || xx < 0 || xx >= input[0].Length || (yy == y && xx == x))
+                        continue;
+
+                    if (input[yy][xx] == '#')
+                        occupiedSeats++;
+                }
+            }
+
+            if (input[y][x] == 'L' && occupiedSeats == 0)
+            {
+                int ly = y, lx = x;
+                changes.Add(() => input[ly][lx] = '#');
+            }
+            else if (input[y][x] == '#' && occupiedSeats >= 4)
+            {
+                int ly = y, lx = x;
+                changes.Add(() => input[ly][lx] = 'L');
+            }
+        }
     }
 
-    int arrangements = numsInRange == 3 ? 7 : 2;
-
-    if (numbers.Length == 1)
-        return arrangements;
-
-    if (numbers[0] - n == 1)
-        arrangements++;
-
-    return arrangements * CountArrangements(numbers[numsInRange], numbers[numsInRange..]);
+    foreach (var change in changes)
+    {
+        change.Invoke();
+    }
 }
+while (changes.Count != 0);
+
+var count = 0;
+for (int y = 0; y < input.Length; y++)
+{
+    for (int x = 0; x < input[0].Length; x++)
+    {
+        if (input[y][x] == '#')
+            count++;
+    }
+}
+
+Console.WriteLine(rounds);
+Console.WriteLine(count);
