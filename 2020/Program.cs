082192b143aa689b389ff7977b546ed1081c1185
Eddie Sundvall <eddie.sundvall@gmail.com>
2020-12-12 08:28:55 +0100

    Add part 2

--- 2020/Program.cs
+++ 2020/Program.cs
@@ -12,78 +12,39 @@ var input = File.ReadAllLines("../../../12.in").Select(l =>
 });
 
 int x = 0, y = 0;
-int dx = 1, dy = 0;
+int wx = 10, wy = -1;
 
 foreach (var instr in input)
 {
     if (instr.direction == 'N')
-        y -= instr.value;
+        wy -= instr.value;
     else if (instr.direction == 'E')
-        x += instr.value;
+        wx += instr.value;
     else if (instr.direction == 'S')
-        y += instr.value;
+        wy += instr.value;
     else if (instr.direction == 'W')
-        x -= instr.value;
+        wx -= instr.value;
     else if (instr.direction == 'F')
     {
-        y += dy * instr.value;
-        x += dx * instr.value;
-    }
-    else if (instr.direction == 'B')
-    {
-        y -= dy * instr.value;
-        x -= dx * instr.value;
+        y += wy * instr.value;
+        x += wx * instr.value;
     }
     else if (instr.direction == 'R')
     {
         for (int i = 0; i < instr.value / 90; i++)
         {
-            if (dx == 1)
-            {
-                dx = 0;
-                dy = 1;
-            }
-            else if (dy == 1)
-            {
-                dx = -1;
-                dy = 0;
-            }
-            else if (dx == -1)
-            {
-                dx = 0;
-                dy = -1;
-            }
-            else if (dy == -1)
-            {
-                dx = 1;
-                dy = 0;
-            }
+            var oldY = wy;
+            wy = wx;
+            wx = -oldY;
         }
     }
     else if (instr.direction == 'L')
     {
         for (int i = 0; i < instr.value / 90; i++)
         {
-            if (dx == 1)
-            {
-                dx = 0;
-                dy = -1;
-            }
-            else if (dy == 1)
-            {
-                dx = 1;
-                dy = 0;
-            }
-            else if (dx == -1)
-            {
-                dx = 0;
-                dy = 1;
-            }
-            else if (dy == -1)
-            {
-                dx = -1;
-                dy = 0;
-            }
+            var oldY = wy;
+            wy = -wx;
+            wx = oldY;
         }
     }
 }
