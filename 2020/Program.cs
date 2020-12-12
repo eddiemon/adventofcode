fc14ae886b0a507863248f60f9073e001640781d
Eddie Sundvall <eddie.sundvall@gmail.com>
2020-12-12 09:53:15 +0100

    Add shameful day 10 part 2

--- 2020/Program.cs
+++ 2020/Program.cs
@@ -4,60 +4,27 @@ using System.IO;
 using System.Linq;
 using System.Numerics;
 using System.Text.RegularExpressions;
+using aoc;
 
-var input = File.ReadAllLines("../../../12.in").Select(l =>
+var input = File.ReadAllLines("../../../10.in").Select(n => int.Parse(n)).ToList();
+
+input.Sort();
+input.Insert(0, 0);
+input.Add(input.Max() + 3);
+
+int a = 0, b = 0, c = 0;
+int ones = 0;
+for (int i = 1; i < input.Count; i++)
 {
-    var m = Regex.Match(l, "(?<dir>.)(?<num>\\d+)");
-    return new Instruction(m.Groups["dir"].Value[0], int.Parse(m.Groups["num"].Value));
-});
-
-int x = 0, y = 0;
-var d = new int[] { 0, 1, 0, -1 };
-int dx = 1, dy = 0;
-
-foreach (var instr in input)
-{
-    if (instr.direction == 'N')
-        y -= instr.value;
-    else if (instr.direction == 'E')
-        x += instr.value;
-    else if (instr.direction == 'S')
-        y += instr.value;
-    else if (instr.direction == 'W')
-        x -= instr.value;
-    else if (instr.direction == 'F')
+    if (input[i] - input[i - 1] == 3)
     {
-        y += d[dy] * instr.value;
-        x += d[dx] * instr.value;
+        if (ones == 4) a++;
+        else if (ones == 3) b++;
+        else if (ones == 2) c++;
+        ones = 0;
     }
-    else if (instr.direction == 'B')
-    {
-        y -= d[dy] * instr.value;
-        x -= d[dx] * instr.value;
-    }
-    else if (instr.direction == 'R')
-    {
-        for (int i = 0; i < instr.value / 90; i++)
-        {
-            dx = mod(dx + 1, d.Length);
-            dy = mod(dy + 1, d.Length);
-        }
-    }
-    else if (instr.direction == 'L')
-    {
-        for (int i = 0; i < instr.value / 90; i++)
-        {
-            dx = mod(dx - 1, d.Length);
-            dy = mod(dy - 1, d.Length);
-        }
-    }
+    else if (input[i] - input[i - 1] == 1)
+        ones++;
 }
 
-Console.WriteLine(Math.Abs(x + y));
-
-int mod(int x, int m)
-{
-    return (x % m + m) % m;
-}
-
-record Instruction(char direction, int value);
+Console.WriteLine(Math.Pow(7, a) * Math.Pow(4, b) * Math.Pow(2, c));
