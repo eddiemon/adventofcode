0e0233ffc75939671dafa59045aed1326efc11ea
Eddie Sundvall <eddie.sundvall@gmail.com>
2020-12-13 09:14:17 +0100

    Add day 13 part 1

--- 2020/Program.cs
+++ 2020/Program.cs
@@ -4,27 +4,10 @@ using System.IO;
 using System.Linq;
 using System.Numerics;
 using System.Text.RegularExpressions;
-using aoc;
 
-var input = File.ReadAllLines("../../../10.in").Select(n => int.Parse(n)).ToList();
+var input = File.ReadAllLines("../../../13.in");
+var estimatedTime = long.Parse(input[0]);
+var busIds = input[1].Split(',').Where(c => c != "x").Select(c => long.Parse(c)).ToArray();
 
-input.Sort();
-input.Insert(0, 0);
-input.Add(input.Max() + 3);
-
-int a = 0, b = 0, c = 0;
-int ones = 0;
-for (int i = 1; i < input.Count; i++)
-{
-    if (input[i] - input[i - 1] == 3)
-    {
-        if (ones == 4) a++;
-        else if (ones == 3) b++;
-        else if (ones == 2) c++;
-        ones = 0;
-    }
-    else if (input[i] - input[i - 1] == 1)
-        ones++;
-}
-
-Console.WriteLine(Math.Pow(7, a) * Math.Pow(4, b) * Math.Pow(2, c));
+var bus = busIds.Select(id => (id, time: id - estimatedTime % id)).OrderBy(n => n.time).First();
+Console.WriteLine(bus.id * bus.time);
