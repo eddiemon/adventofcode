74b7256efe2b7a80a9571b7d4f72f7e37f21598e
Eddie Sundvall <eddie.sundvall@gmail.com>
2020-12-16 08:16:31 +0100

    Add day 16 part 1

--- 2020/Program.cs
+++ 2020/Program.cs
@@ -6,45 +6,61 @@ using System.Numerics;
 using System.Text;
 using System.Text.RegularExpressions;
 
-//var input = File.ReadAllLines("../../../14.in");
-var spoken = new Dictionary<int, List<int>>();
+var input = File.ReadAllLines("../../../16.in");
 
-int turn = 1;
-int lastSpoken = 0;
-Say(0);
-Say(5);
-Say(4);
-Say(1);
-Say(10);
-Say(14);
-Say(7);
+var readingMyTicket = false;
+var readingNearbyTickets = false;
 
-//Say(0);
-//Say(3);
-//Say(6);
+var rules = new Dictionary<string, Func<int, bool>>();
+var ticketScanningErrorRate = 0;
 
-while (turn < 30000000)
+foreach (var l in input)
 {
-    Say(WhatToSay());
+    if (string.IsNullOrEmpty(l))
+        continue;
+    if (l == "your ticket:")
+    {
+        readingMyTicket = true;
+        continue;
+    }
+    if (l == "nearby tickets:")
+    {
+        readingNearbyTickets = true;
+        continue;
+    }
+
+    if (readingNearbyTickets)
+    {
+        var t = l.Split(',').Select(i => int.Parse(i));
+        foreach (var n in t)
+        {
+            bool found = false;
+            foreach (var (rule, validate) in rules)
+            {
+                if (validate(n))
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+                ticketScanningErrorRate += n;
+        }
+    }
+    else if (readingMyTicket)
+    {
+    }
+    else
+    {
+        var m = Regex.Match(l, @"^(?<rule>.+): (?<min1>\d+)\-(?<max1>\d+) or (?<min2>\d+)\-(?<max2>\d+)$");
+        var rule = m.Groups["rule"].Value;
+        var min1 = int.Parse(m.Groups["min1"].Value);
+        var max1 = int.Parse(m.Groups["max1"].Value);
+        var min2 = int.Parse(m.Groups["min2"].Value);
+        var max2 = int.Parse(m.Groups["max2"].Value);
+        Func<int, bool> validation = (int n) => n >= min1 && n <= max1 || n >= min2 && n <= max2;
+        rules.Add(rule, validation);
+    }
 }
 
-Console.WriteLine(WhatToSay());
-
-int WhatToSay()
-{
-    if (spoken[lastSpoken].Count == 1)
-        return 0;
-    var last = spoken[lastSpoken][^1];
-    var secondLast = spoken[lastSpoken][^2];
-    return last - secondLast;
-}
-
-void Say(int n)
-{
-    if (!spoken.ContainsKey(n))
-        spoken[n] = new List<int>();
-
-    spoken[n].Add(turn);
-    lastSpoken = n;
-    turn++;
-}
+Console.WriteLine(ticketScanningErrorRate);
