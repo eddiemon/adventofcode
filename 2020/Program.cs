917d8d9c04c26f183bf1dd27479c145b89592c8d
Eddie Sundvall <eddie.sundvall@gmail.com>
2020-12-16 22:04:47 +0100

    Add part 2

--- 2020/Program.cs
+++ 2020/Program.cs
@@ -12,7 +12,9 @@ var readingMyTicket = false;
 var readingNearbyTickets = false;
 
 var rules = new Dictionary<string, Func<int, bool>>();
-var ticketScanningErrorRate = 0;
+
+int[] myTicket = new int[0];
+var nearbyTickets = new List<int[]>();
 
 foreach (var l in input)
 {
@@ -31,24 +33,13 @@ foreach (var l in input)
 
     if (readingNearbyTickets)
     {
-        var t = l.Split(',').Select(i => int.Parse(i));
-        foreach (var n in t)
-        {
-            bool found = false;
-            foreach (var (rule, validate) in rules)
-            {
-                if (validate(n))
-                {
-                    found = true;
-                    break;
-                }
-            }
-            if (!found)
-                ticketScanningErrorRate += n;
-        }
+        var t = l.Split(',').Select(i => int.Parse(i)).ToArray();
+        nearbyTickets.Add(t);
     }
     else if (readingMyTicket)
     {
+        var t = l.Split(',').Select(i => int.Parse(i)).ToArray();
+        myTicket = t;
     }
     else
     {
@@ -58,9 +49,46 @@ foreach (var l in input)
         var max1 = int.Parse(m.Groups["max1"].Value);
         var min2 = int.Parse(m.Groups["min2"].Value);
         var max2 = int.Parse(m.Groups["max2"].Value);
-        Func<int, bool> validation = (int n) => n >= min1 && n <= max1 || n >= min2 && n <= max2;
+        Func<int, bool> validation = (int n) => (n >= min1 && n <= max1) || (n >= min2 && n <= max2);
         rules.Add(rule, validation);
     }
 }
 
-Console.WriteLine(ticketScanningErrorRate);
+var validNearbyTickets = nearbyTickets.Where(t => TicketIsValid(t)).ToList();
+
+var validations = rules.Select(kvp => kvp.Value).ToList();
+
+var columnValidators = new Dictionary<Func<int, bool>, int>();
+while (validations.Count > 0)
+{
+    for (int i = 0; i < myTicket.Length; i++)
+    {
+        var fieldIsDepartureField = validations.Where(v => validNearbyTickets.All(t => v(t[i]))).ToList();
+        if (fieldIsDepartureField.Count == 1)
+        {
+            validations.Remove(fieldIsDepartureField.First());
+            columnValidators.Add(fieldIsDepartureField.First(), i);
+            break;
+        }
+    }
+}
+
+var departureValidations = rules.Where(kvp => kvp.Key.Contains("departure")).Select(kvp => kvp.Value).ToList();
+var product = 1L;
+foreach (var validation in departureValidations)
+{
+    var column = columnValidators[validation];
+    product *= myTicket[column];
+}
+Console.WriteLine(product);
+
+bool TicketIsValid(int[] t)
+{
+    foreach (var n in t)
+    {
+        var isValid = rules.Values.Any(valid => valid(n));
+        if (!isValid)
+            return false;
+    }
+    return true;
+}
