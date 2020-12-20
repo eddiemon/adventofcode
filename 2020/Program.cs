01e880e97398d1416ad2bd16edd1042ee6c85015
Eddie Sundvall <eddie.sundvall@gmail.com>
2020-12-20 19:35:03 +0100

    Solve part 1

--- 2020/Program.cs
+++ 2020/Program.cs
@@ -29,231 +29,353 @@ foreach (var l in input)
     }
 }
 
-var placedTiles = new Dictionary<Vector2, Tile>();
-
-var tile = unplacedTiles.First();
-
-PlaceAdjecentTiles(tile, 0, 0);
-
-void PlaceAdjecentTiles(Tile tile, int x, int y)
+var adjecentTileCandidates = new Dictionary<Tile, List<List<Tile>>>();
+foreach (var tile in unplacedTiles)
 {
-    if (placedTiles.Keys.Contains((x, y)))
-    {
-        Console.WriteLine("Please come again");
-        return;
-    }
+    adjecentTileCandidates.Add(tile, FindAdjecentTileCandidates(tile));
+}
 
-    placedTiles.Add((x, y), tile);
-    unplacedTiles.Remove(tile);
+var edgeTiles = adjecentTileCandidates.Where(kvp => kvp.Value.Any(adjTiles => adjTiles.Count == 0)).Select(kvp => kvp.Key).ToList();
+var cornerTiles = edgeTiles.Where(t => adjecentTileCandidates[t].Where(adjTiles => adjTiles.Count == 0).Count() == 2).ToList();
+Console.WriteLine(edgeTiles.Count);
+Console.WriteLine(cornerTiles.Select(t => t.Id).Aggregate((a, b) => a * b));
+
+//var placedTiles = new Dictionary<Vector2, Tile>();
+
+//PlaceAdjecentTiles(cornerTiles.First(), 0, 0);
+
+//var minX = placedTiles.Keys.Min(u => u.x);
+//var maxX = placedTiles.Keys.Max(u => u.x);
+//var minY = placedTiles.Keys.Min(u => u.y);
+//var maxY = placedTiles.Keys.Max(u => u.y);
+
+//for (int y = minY; y <= maxY; y++)
+//{
+//    for (int x = minX; x <= maxX; x++)
+//    {
+//        var tileKey = placedTiles.Keys.FirstOrDefault(t => t.x == x && t.y == y);
+//        var tId = placedTiles[tileKey].Id.ToString() ?? "9999";
+//        Console.Write($" {tId} ");
+//    }
+//    Console.WriteLine();
+//}
+
+//var topLeftTile = placedTiles[(minX, minY)];
+//var topRightTile = placedTiles[(maxX, minY)];
+//var bottomLeftTile = placedTiles[(minX, maxY)];
+//var bottomRightTile = placedTiles[(maxX, maxY)];
+
+//Console.WriteLine(topLeftTile.Id * topRightTile.Id * bottomLeftTile.Id * bottomRightTile.Id);
+
+List<List<Tile>> FindAdjecentTileCandidates(Tile tile)
+{
+    var north = new List<Tile>();
+    var south = new List<Tile>();
+    var east = new List<Tile>();
+    var west = new List<Tile>();
+    var result = new List<List<Tile>> { north, south, east, west };
 
     var dir = tile.GetNorth();
-    foreach (var t in UnplacedTiles())
+    foreach (var t in UnplacedTiles(tile))
     {
         if (t.GetWest() == dir)
-        {
-            PlaceAdjecentTiles(t.RotateCCW(), x, y - 1);
-            //break;
-        }
+            north.Add(t.RotateCCW());
         else if (t.GetWestReverse() == dir)
-        {
-            PlaceAdjecentTiles(t.RotateCCW().FlipHorizontal(), x, y - 1);
-            //break;
-        }
+            north.Add(t.RotateCCW().FlipHorizontal());
         else if (t.GetEast() == dir)
-        {
-            PlaceAdjecentTiles(t.RotateCW().FlipVertical(), x, y - 1);
-            //break;
-        }
+            north.Add(t.RotateCW().FlipVertical());
         else if (t.GetEastReverse() == dir)
-        {
-            PlaceAdjecentTiles(t.RotateCW(), x, y - 1);
-            //break;
-        }
+            north.Add(t.RotateCW());
         else if (t.GetNorth() == dir)
-        {
-            PlaceAdjecentTiles(t.FlipHorizontal(), x, y - 1);
-            //break;
-        }
+            north.Add(t.FlipHorizontal());
         else if (t.GetNorthReverse() == dir)
-        {
-            PlaceAdjecentTiles(t.FlipHorizontal().FlipVertical(), x, y - 1);
-            //break;
-        }
+            north.Add(t.FlipHorizontal().FlipVertical());
         else if (t.GetSouth() == dir)
-        {
-            PlaceAdjecentTiles(t, x, y - 1);
-            //break;
-        }
+            north.Add(t);
         else if (t.GetSouthReverse() == dir)
-        {
-            PlaceAdjecentTiles(t.FlipVertical(), x, y - 1);
-            //break;
-        }
+            north.Add(t.FlipVertical());
     }
 
     dir = tile.GetSouth();
-    foreach (var t in UnplacedTiles())
+    foreach (var t in UnplacedTiles(tile))
     {
         if (t.GetWest() == dir)
-        {
-            PlaceAdjecentTiles(t.RotateCW().FlipVertical(), x, y + 1);
-            //break;
-        }
+            south.Add(t.RotateCW().FlipVertical());
         else if (t.GetWestReverse() == dir)
-        {
-            PlaceAdjecentTiles(t.RotateCW(), x, y + 1);
-            //break;
-        }
+            south.Add(t.RotateCW());
         else if (t.GetEast() == dir)
-        {
-            PlaceAdjecentTiles(t.RotateCCW(), x, y + 1);
-            //break;
-        }
+            south.Add(t.RotateCCW());
         else if (t.GetEastReverse() == dir)
-        {
-            PlaceAdjecentTiles(t.RotateCCW().FlipVertical(), x, y + 1);
-            //break;
-        }
+            south.Add(t.RotateCCW().FlipVertical());
         else if (t.GetNorth() == dir)
-        {
-            PlaceAdjecentTiles(t, x, y + 1);
-            //break;
-        }
+            south.Add(t);
         else if (t.GetNorthReverse() == dir)
-        {
-            PlaceAdjecentTiles(t.FlipVertical(), x, y + 1);
-            //break;
-        }
+            south.Add(t.FlipVertical());
         else if (t.GetSouth() == dir)
-        {
-            PlaceAdjecentTiles(t.FlipHorizontal(), x, y + 1);
-            //break;
-        }
+            south.Add(t.FlipHorizontal());
         else if (t.GetSouthReverse() == dir)
-        {
-            PlaceAdjecentTiles(t.FlipHorizontal().FlipVertical(), x, y + 1);
-            //break;
-        }
+            south.Add(t.FlipHorizontal().FlipVertical());
     }
 
     dir = tile.GetEast();
-    foreach (var t in UnplacedTiles())
+    foreach (var t in UnplacedTiles(tile))
     {
         if (t.GetWest() == dir)
-        {
-            PlaceAdjecentTiles(t, x + 1, y);
-            //break;
-        }
+            east.Add(t);
         else if (t.GetWestReverse() == dir)
-        {
-            PlaceAdjecentTiles(t.FlipHorizontal(), x + 1, y);
-            //break;
-        }
+            east.Add(t.FlipHorizontal());
         else if (t.GetEast() == dir)
-        {
-            PlaceAdjecentTiles(t.FlipVertical(), x + 1, y);
-            //break;
-        }
+            east.Add(t.FlipVertical());
         else if (t.GetEastReverse() == dir)
-        {
-            PlaceAdjecentTiles(t.FlipVertical().FlipHorizontal(), x + 1, y);
-            //break;
-        }
+            east.Add(t.FlipVertical().FlipHorizontal());
         else if (t.GetNorth() == dir)
-        {
-            PlaceAdjecentTiles(t.RotateCCW().FlipVertical(), x + 1, y);
-            //break;
-        }
+            east.Add(t.RotateCCW().FlipVertical());
         else if (t.GetNorthReverse() == dir)
-        {
-            PlaceAdjecentTiles(t.RotateCCW(), x + 1, y);
-            //break;
-        }
+            east.Add(t.RotateCCW());
         else if (t.GetSouth() == dir)
-        {
-            PlaceAdjecentTiles(t.RotateCW(), x + 1, y);
-            //break;
-        }
+            east.Add(t.RotateCW());
         else if (t.GetSouthReverse() == dir)
-        {
-            PlaceAdjecentTiles(t.RotateCW().FlipVertical(), x + 1, y);
-            //break;
-        }
+            east.Add(t.RotateCW().FlipVertical());
     }
 
     dir = tile.GetWest();
-    foreach (var t in UnplacedTiles())
+    foreach (var t in UnplacedTiles(tile))
     {
         if (t.GetWest() == dir)
-        {
-            PlaceAdjecentTiles(t.FlipVertical(), x - 1, y);
-            //break;
-        }
+            west.Add(t.FlipVertical());
         else if (t.GetWestReverse() == dir)
-        {
-            PlaceAdjecentTiles(t.FlipVertical().FlipHorizontal(), x - 1, y);
-            //break;
-        }
+            west.Add(t.FlipVertical().FlipHorizontal());
         else if (t.GetEast() == dir)
-        {
-            PlaceAdjecentTiles(t, x - 1, y);
-            //break;
-        }
+            west.Add(t);
         else if (t.GetEastReverse() == dir)
-        {
-            PlaceAdjecentTiles(t.FlipHorizontal(), x - 1, y);
-            //break;
-        }
+            west.Add(t.FlipHorizontal());
         else if (t.GetNorth() == dir)
-        {
-            PlaceAdjecentTiles(t.RotateCW(), x - 1, y);
-            //break;
-        }
+            west.Add(t.RotateCW());
         else if (t.GetNorthReverse() == dir)
-        {
-            PlaceAdjecentTiles(t.RotateCW().FlipVertical(), x - 1, y);
-            //break;
-        }
+            west.Add(t.RotateCW().FlipVertical());
         else if (t.GetSouth() == dir)
-        {
-            PlaceAdjecentTiles(t.RotateCCW().FlipVertical(), x - 1, y);
-            //break;
-        }
+            west.Add(t.RotateCCW().FlipVertical());
         else if (t.GetSouthReverse() == dir)
-        {
-            PlaceAdjecentTiles(t.RotateCCW(), x - 1, y);
-            //break;
-        }
+            west.Add(t.RotateCCW());
     }
+
+    return result;
 }
 
-var minX = placedTiles.Keys.Min(u => u.x);
-var maxX = placedTiles.Keys.Max(u => u.x);
-var minY = placedTiles.Keys.Min(u => u.y);
-var maxY = placedTiles.Keys.Max(u => u.y);
-
-for (int y = minY; y <= maxY; y++)
+void PlaceAdjecentTiles(Tile tile, int x, int y)
 {
-    for (int x = minX; x <= maxX; x++)
-    {
-        var tileKey = placedTiles.Keys.FirstOrDefault(t => t.x == x && t.y == y);
-        var tId = placedTiles[tileKey].Id.ToString() ?? "9999";
-        Console.Write($" {tId} ");
-    }
-    Console.WriteLine();
+    //if (placedTiles.Keys.Contains((x, y)))
+    //    throw new Exception();
+
+    //var northCandidates = adjecentTileCandidates[tile][0];
+    //var southCandidates = adjecentTileCandidates[tile][1];
+    //var eastCandidates = adjecentTileCandidates[tile][2];
+    //var westCandidates = adjecentTileCandidates[tile][3];
+
+    ////var dir = tile.GetNorth();
+    //while (northCandidates.Count > 1)
+    //{
+    //    var candidateTile = northCandidates.First();
+    //    try
+    //    {
+    //        PlaceAdjecentTiles(candidateTile, x, y - 1);
+    //    }
+    //    catch
+    //    {
+    //        northCandidates.Remove(candidateTile);
+    //        placedTiles.Remove((x, y - 1));
+    //    }
+    //}
+
+    //placedTiles.Add((x, y), tile);
+    //unplacedTiles.Remove(tile);
+
+    //foreach (var t in UnplacedTiles())
+    //{
+    //    if (t.GetWest() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.RotateCCW(), x, y - 1);
+    //        //break;
+    //    }
+    //    else if (t.GetWestReverse() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.RotateCCW().FlipHorizontal(), x, y - 1);
+    //        //break;
+    //    }
+    //    else if (t.GetEast() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.RotateCW().FlipVertical(), x, y - 1);
+    //        //break;
+    //    }
+    //    else if (t.GetEastReverse() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.RotateCW(), x, y - 1);
+    //        //break;
+    //    }
+    //    else if (t.GetNorth() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.FlipHorizontal(), x, y - 1);
+    //        //break;
+    //    }
+    //    else if (t.GetNorthReverse() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.FlipHorizontal().FlipVertical(), x, y - 1);
+    //        //break;
+    //    }
+    //    else if (t.GetSouth() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t, x, y - 1);
+    //        //break;
+    //    }
+    //    else if (t.GetSouthReverse() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.FlipVertical(), x, y - 1);
+    //        //break;
+    //    }
+    //}
+
+    //dir = tile.GetSouth();
+    //foreach (var t in UnplacedTiles())
+    //{
+    //    if (t.GetWest() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.RotateCW().FlipVertical(), x, y + 1);
+    //        //break;
+    //    }
+    //    else if (t.GetWestReverse() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.RotateCW(), x, y + 1);
+    //        //break;
+    //    }
+    //    else if (t.GetEast() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.RotateCCW(), x, y + 1);
+    //        //break;
+    //    }
+    //    else if (t.GetEastReverse() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.RotateCCW().FlipVertical(), x, y + 1);
+    //        //break;
+    //    }
+    //    else if (t.GetNorth() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t, x, y + 1);
+    //        //break;
+    //    }
+    //    else if (t.GetNorthReverse() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.FlipVertical(), x, y + 1);
+    //        //break;
+    //    }
+    //    else if (t.GetSouth() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.FlipHorizontal(), x, y + 1);
+    //        //break;
+    //    }
+    //    else if (t.GetSouthReverse() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.FlipHorizontal().FlipVertical(), x, y + 1);
+    //        //break;
+    //    }
+    //}
+
+    //dir = tile.GetEast();
+    //foreach (var t in UnplacedTiles())
+    //{
+    //    if (t.GetWest() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t, x + 1, y);
+    //        //break;
+    //    }
+    //    else if (t.GetWestReverse() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.FlipHorizontal(), x + 1, y);
+    //        //break;
+    //    }
+    //    else if (t.GetEast() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.FlipVertical(), x + 1, y);
+    //        //break;
+    //    }
+    //    else if (t.GetEastReverse() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.FlipVertical().FlipHorizontal(), x + 1, y);
+    //        //break;
+    //    }
+    //    else if (t.GetNorth() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.RotateCCW().FlipVertical(), x + 1, y);
+    //        //break;
+    //    }
+    //    else if (t.GetNorthReverse() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.RotateCCW(), x + 1, y);
+    //        //break;
+    //    }
+    //    else if (t.GetSouth() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.RotateCW(), x + 1, y);
+    //        //break;
+    //    }
+    //    else if (t.GetSouthReverse() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.RotateCW().FlipVertical(), x + 1, y);
+    //        //break;
+    //    }
+    //}
+
+    //dir = tile.GetWest();
+    //foreach (var t in UnplacedTiles())
+    //{
+    //    if (t.GetWest() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.FlipVertical(), x - 1, y);
+    //        //break;
+    //    }
+    //    else if (t.GetWestReverse() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.FlipVertical().FlipHorizontal(), x - 1, y);
+    //        //break;
+    //    }
+    //    else if (t.GetEast() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t, x - 1, y);
+    //        //break;
+    //    }
+    //    else if (t.GetEastReverse() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.FlipHorizontal(), x - 1, y);
+    //        //break;
+    //    }
+    //    else if (t.GetNorth() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.RotateCW(), x - 1, y);
+    //        //break;
+    //    }
+    //    else if (t.GetNorthReverse() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.RotateCW().FlipVertical(), x - 1, y);
+    //        //break;
+    //    }
+    //    else if (t.GetSouth() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.RotateCCW().FlipVertical(), x - 1, y);
+    //        //break;
+    //    }
+    //    else if (t.GetSouthReverse() == dir)
+    //    {
+    //        PlaceAdjecentTiles(t.RotateCCW(), x - 1, y);
+    //        //break;
+    //    }
+    //}
 }
 
-var topLeftTile = placedTiles[(minX, minY)];
-var topRightTile = placedTiles[(maxX, minY)];
-var bottomLeftTile = placedTiles[(minX, maxY)];
-var bottomRightTile = placedTiles[(maxX, maxY)];
-
-Console.WriteLine(topLeftTile.Id * topRightTile.Id * bottomLeftTile.Id * bottomRightTile.Id);
-
-IEnumerable<Tile> UnplacedTiles()
+IEnumerable<Tile> UnplacedTiles(Tile? excluded = null)
 {
     for (int i = 0; i < unplacedTiles.Count; i++)
     {
+        if (excluded != null && excluded == unplacedTiles[i])
+            continue;
         yield return unplacedTiles[i];
     }
 }
