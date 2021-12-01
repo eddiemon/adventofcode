c9e65106b065b5546aa73168062cabb2f23106ce
Eddie Sundvall <eddie.sundvall@gmail.com>
2021-12-01 08:50:54 +0100

    Add 2021 day 1 part 2

--- 2021/1.fsx
+++ 2021/1.fsx
@@ -7,4 +7,15 @@ let noOfIncrements =
     (0, depths, depths |> Seq.tail)
     |||> Seq.fold2 (fun acc x1 x2 -> if x2 > x1 then acc + 1 else acc)
 
-printfn "%A" noOfIncrements
+printfn "Part 1: %A" noOfIncrements
+
+let depthsWindowed =
+    depths
+    |> Seq.windowed 3
+    |> Seq.map Seq.sum
+
+let noOfIncrements2 =
+    (0, depthsWindowed, depthsWindowed |> Seq.tail)
+    |||> Seq.fold2 (fun acc x1 x2 -> if x2 > x1 then acc + 1 else acc)
+
+printfn "Part 2: %A" noOfIncrements2
