de1a660ca192715d59ec39a784a015d99fb7b033
Eddie Sundvall <eddie.sundvall@gmail.com>
2021-12-02 08:03:53 +0100

    Simplify code a bit

--- 2021/2.fsx
+++ 2021/2.fsx
@@ -1,6 +1,5 @@
 
 type Direction = Forward | Up | Down
-type Command = { Direction: Direction; Magnitude: int }
 
 let split (delimiter:char) (theString:string) = theString.Split(delimiter)
 
@@ -8,28 +7,28 @@ let commands =
     System.IO.File.ReadAllLines("2.txt")
     |> Seq.map (split ' ')
     |> Seq.map (fun s -> match s[0] with
-                            | "forward" -> { Direction = Forward; Magnitude = int(s[1])}
-                            | "up" -> { Direction = Up; Magnitude = int(s[1])}
-                            | "down" -> { Direction = Down; Magnitude = int(s[1])}
+                            | "forward" -> (Forward, int(s[1]))
+                            | "up" -> (Up, int(s[1]))
+                            | "down" -> (Down, int(s[1]))
     )
 
-let depths =
-    ([|0;0|], commands)
-    ||> Seq.fold (fun acc x -> match x.Direction with
-                                | Forward -> [|acc[0] + x.Magnitude; acc.[1]|]
-                                | Up ->      [|acc[0]; acc.[1] - x.Magnitude|]
-                                | Down ->    [|acc[0]; acc.[1] + x.Magnitude|]
+let (h,d) =
+    ((0,0), commands)
+    ||> Seq.fold (fun (h,d) (dir,magn) -> match dir with
+                                    | Forward -> (h + magn, d)
+                                    | Up ->      (h       , d - magn)
+                                    | Down ->    (h       , d + magn)
     )
 
-printfn "Part 1: %A" (depths[0]*depths[1])
+printfn "Part 1: %A" (h*d)
 
-let depths2 =
-    ([|0;0;0|], commands)
-    ||> Seq.fold (fun acc x -> match x.Direction with
-                                | Forward -> [|acc[0] + x.Magnitude; acc[1] + acc[2]*x.Magnitude; acc[2]|]
-                                | Up ->      [|acc[0]; acc[1]; acc[2] - x.Magnitude|]
-                                | Down ->    [|acc[0]; acc[1]; acc[2] + x.Magnitude|]
+let (h2, d2, a) =
+    ((0,0,0), commands)
+    ||> Seq.fold (fun (h,d,a) (dir,magn) -> match dir with
+                                    | Forward -> (h + magn, d + a * magn, a)
+                                    | Up ->      (h       , d           , a - magn)
+                                    | Down ->    (h       , d           , a + magn)
     )
 
-printfn "Part 2: %A" (depths2[0]*depths2[1])
+printfn "Part 2: %A" (h2*d2)
 
