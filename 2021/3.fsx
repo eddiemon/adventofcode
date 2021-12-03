6bae262821ca7706308d00c3b83a60d130420798
Eddie Sundvall <eddie.sundvall@gmail.com>
2021-12-03 22:39:58 +0100

    Add 2021 day 3 part 2

--- 2021/3.fsx
+++ 2021/3.fsx
@@ -1,21 +1,22 @@
 
-let digits = System.IO.File.ReadAllLines("3.txt")
+let digits = Seq.ofArray (System.IO.File.ReadAllLines("3.txt")) |> Seq.map (fun s -> s.ToCharArray() |> Seq.map System.Char.GetNumericValue |> Seq.map int)
 
 let tdigits =
     digits
     |> Seq.transpose
-    |> Seq.map (fun s -> s |> Seq.map System.Char.GetNumericValue |> Seq.map int)
 
 let mcd digits = // most common digit
     digits
     |> Seq.countBy (fun x -> x)
-    |> Seq.maxBy (fun x -> snd(x))
+    |> Seq.sortByDescending fst
+    |> Seq.maxBy snd
     |> fst
 
 let lcd digits = // least common digit
     digits
     |> Seq.countBy (fun x -> x)
-    |> Seq.minBy (fun x -> snd(x))
+    |> Seq.sortBy fst
+    |> Seq.minBy snd
     |> fst
 
 let toDecimal digits =
@@ -35,3 +36,22 @@ let epsilon =
     |> toDecimal
 
 printfn "Part 1: %A" (gamma * epsilon)
+
+let bitPredicate index (filterBit:int) seq = Seq.filter (fun b -> Seq.item index b = filterBit) seq
+
+let rating filterFunc =
+    [0..Seq.length tdigits]
+    |>  Seq.fold (fun (acc, index) _ ->
+        let filterBit = acc |> Seq.transpose |> Seq.item index |> filterFunc
+        let filteredDigits = bitPredicate index filterBit acc
+        (filteredDigits, index + 1)
+    ) (digits, 0)
+    |> fst
+    |> Seq.head
+    |> toDecimal
+
+
+let oxygenRating = rating mcd
+let co2scrubRating = rating lcd
+
+co2scrubRating * oxygenRating
