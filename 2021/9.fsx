c11be0a0c952b1412bcdd75d61b1a61ea1e57d3f
Eddie Sundvall <eddie.sundvall@gmail.com>
2021-12-10 17:53:59 +0100

    Add 2021 day 9 part 2

--- 2021/9.fsx
+++ 2021/9.fsx
@@ -5,21 +5,21 @@ let heightmap =
     |> Seq.map (fun s -> s |> Seq.map (fun c -> System.Char.GetNumericValue c |> int) |> Array.ofSeq)
     |> Array.ofSeq
 
-
-let lowest y x =
-    let height = heightmap[y][x]
-    Seq.zip [-1;1;0;0] [0;0;-1;1]
-    |> Seq.map (fun (dy, dx) ->
-        // printfn "(%i, %i)" (x+dx) (y+dy)
-        (y+dy, x+dx)
+let neighbouringPositions (x,y) =
+    [(0, -1); (0, 1); (-1, 0); (1, 0)]
+    |> Seq.map (fun (dx, dy) ->
+        (x+dx, y+dy)
     )
     |> Seq.filter (
         function
-        | (y, x) when y < 0 || x < 0 || y >= Seq.length heightmap || x >= Seq.length heightmap[0] -> false
+        | (x, y) when y < 0 || x < 0 || y >= Seq.length heightmap || x >= Seq.length heightmap[y] -> false
         | _ -> true
     )
-    |> Seq.map (fun (yy, xx) -> 
-        // printfn "%i (%i,%i) is adjecent to (%i,%i)" (heightmap[yy][xx]) xx yy x y
+
+let lowest (x, y) =
+    let height = heightmap[y][x]
+    neighbouringPositions (x, y)
+    |> Seq.map (fun (xx, yy) ->
         heightmap[yy][xx]
     )
     |> Seq.exists (fun adjecentHeight -> adjecentHeight <= height)
@@ -29,8 +29,7 @@ let lowestHeights =
     let xy = Seq.allPairs [0..Seq.length heightmap - 1] [0..Seq.length heightmap[0] - 1]
     
     Seq.fold (fun list (y, x) ->
-        if lowest y x then
-            // printfn "%i (%i,%i) is lower than adjecents" (heightmap[y][x]) x y
+        if lowest (x, y) then
             list@[((heightmap[y][x]), (x, y))]
         else
             list
@@ -41,8 +40,33 @@ let result1 =
     |> List.map (fun x -> fst x + 1)
     |> List.sum
 
+printfn "%A" result1
+
+let dequeue (l:byref<_>) =
+    let r = List.head l
+    l <- List.skip 1 l
+    r
+
+let enqueue (l:byref<_>) value =
+    l <- l@[value]
+
+let getBasinPositions (x, y) =
+    let mutable discovered = []
+    let mutable S = []
+    S <- S@[(x,y)]
+    while not (List.isEmpty S) do
+        let (x, y) = dequeue &S
+        if not (Seq.contains (x, y) discovered) then
+            enqueue &discovered (x, y)
+            let neighbours = neighbouringPositions (x, y)
+            let asd = Seq.filter (fun (xx,yy) -> heightmap[yy][xx] < 9) neighbours
+            S <- S@(List.ofSeq asd)
+    discovered
+
+
 let getBasinSize (x, y) : int =
-    0
+    getBasinPositions (x,y)
+    |> List.length
 
 let largestBasins =
     lowestHeights
