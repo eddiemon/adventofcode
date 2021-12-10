9cc2d6bd3447260964c37e3b2a98ce9ea11f36e5
Eddie Sundvall <eddie.sundvall@securitas.com>
2021-12-10 23:45:25 +0100

    Add 2021 day 10 part 2

--- 2021/10.fsx
+++ 2021/10.fsx
@@ -2,11 +2,11 @@ let input = System.IO.File.ReadAllLines "10.txt"
 
 let points ch =
     match ch with
-    | Some ')' -> 3L
-    | Some ']' -> 57L
-    | Some '}' -> 1197L
-    | Some '>' -> 25137L
-    | Some _ | None -> 0L
+    | ')' -> 3L
+    | ']' -> 57L
+    | '}' -> 1197L
+    | '>' -> 25137L
+    | _ -> 0L
 
 let opposite ch =
     match ch with
@@ -14,6 +14,10 @@ let opposite ch =
     | ']' -> '['
     | '}' -> '{'
     | '>' -> '<'
+    | '(' -> ')'
+    | '{' -> '}'
+    | '[' -> ']'
+    | '<' -> '>'
     | _ -> ' '
 
 let processLine line =
@@ -37,16 +41,60 @@ let firstIllegalChar line =
     | Some (c, _) -> Some c
     | None -> None
 
-
-// printfn "%A" (firstIllegalChar "{([(<{}[<>[]}>{[]{[(<()>") // }
-// printfn "%A" (firstIllegalChar "[[<[([]))<([[{}[[()]]]" )// )
-// printfn "%A" (firstIllegalChar "[{[{({}]{}}([{[{{{}}([]") // ]
-// printfn "%A" (firstIllegalChar "[<(<(<(<{}))><([]([]()" )// )
-// printfn "%A" (firstIllegalChar "<{([([[(<>()){}]>(<<{{" )// >
+let maybePoints chOpt =
+    match chOpt with
+    | Some c -> points c
+    | None -> 0
 
 let syntaxErrorScore =
     input
     |> Seq.map firstIllegalChar
-    |> Seq.sumBy points
+    |> Seq.sumBy maybePoints
 
 printfn "%A" syntaxErrorScore
+
+let incompleteInput =
+    input
+    |> Seq.filter (fun line ->
+        None = firstIllegalChar line
+    )
+    |> List.ofSeq
+
+let incompleteChars line =
+    let mutable S = []
+    for c in line do
+        match c with
+        | '(' | '{' | '[' | '<' ->
+            S <- c :: S
+        | ')' | '}' | ']' | '>' ->
+            S <- List.tail S
+        | _ -> ignore 0
+    S
+    
+let getCompletion incompleteChars =
+    incompleteChars
+    |> Seq.map opposite
+
+let points2 ch =
+    match ch with
+    | ')' -> 1L
+    | ']' -> 2L
+    | '}' -> 3L
+    | '>' -> 4L
+    | _ -> 0L
+
+let scoreCompletion completion =
+    completion
+    |> Seq.fold (fun acc c ->
+        acc * 5L + (points2 c)
+    ) 0L
+
+let completionScores =
+    incompleteInput
+    |> Seq.map incompleteChars
+    |> Seq.map getCompletion
+    |> Seq.map scoreCompletion
+
+completionScores
+|> Seq.sort
+|> Seq.item ((Seq.length completionScores) / 2)
