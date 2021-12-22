66eff595b2db856147122c841734dab3c5e36b55
Eddie Sundvall <eddie.sundvall@gmail.com>
2021-12-22 17:02:16 +0100

    Add 2021 day 20 part 1&2

--- 2021/20.fsx
+++ 2021/20.fsx
@@ -1,5 +1,11 @@
 let input = System.IO.File.ReadAllLines "20.txt"
 
+type State = {
+    Image : Map<int*int, int>
+    Algorithm: int array
+    InfinitePixelValue: int
+}
+
 let charToInt =
     function
     | '#' -> 1
@@ -16,7 +22,7 @@ let printImage image =
         | 0 -> '.'
 
     let keys = Map.keys image
-    let minX = (Seq.minBy (fun (x,y) -> x) keys) |> fst
+    let minX = Seq.minBy (fun (x,y) -> x) keys |> fst
     let maxX = Seq.maxBy (fun (x,y) -> x) keys |> fst
     let minY = Seq.minBy (fun (x,y) -> y) keys |> snd
     let maxY = Seq.maxBy (fun (x,y) -> y) keys |> snd
@@ -27,40 +33,48 @@ let printImage image =
             | None -> printf "."
         printfn ""
 
-let getPixelValueOrZero image pixel =
+let getPixelValueOrZero image pixelDefault pixel =
     match Map.tryFind pixel image with
     | Some p -> p
-    | None -> 0
+    | None -> pixelDefault
 
 let getDecimal binaryValues =
-    // printfn "%A" binaryValues
     let result =
         Seq.foldBack (fun b (acc, multiplier) ->
             (acc + (b*multiplier), multiplier * 2)
         ) binaryValues (0, 1)
     fst result
 
-let enhance algorithm image =
+let nextInfinitePixelValue = function | 1 -> 0 | 0 -> 1
+
+let enhance state: State =
     let toVisit =
-        image
+        state.Image
         |> Map.keys
         |> Seq.fold (fun toVisit n ->
-            let nn = n |> window9x9 //Seq.fold (fun s nn -> Set.add nn s) toVisit
+            let nn = n |> window9x9
             Seq.fold (fun toVisit nnn -> Set.add nnn toVisit) toVisit nn
         ) Set.empty
         |> List.ofSeq
 
-    Seq.fold (fun image (x,y) ->
-        let pixels = window9x9 (x,y)
-        // printfn "%A" image
-        let algorithmIndex =
-            pixels
-            |> Seq.map (getPixelValueOrZero image)
-            |> getDecimal
-        let newValue = Array.get algorithm algorithmIndex
-        printfn "Found value <%i> in algorithm index <%i> for point (%i, %i)" newValue algorithmIndex x y
-        Map.add (x,y) newValue image
-    ) image toVisit
+    let newImage =
+        Seq.fold (fun updatedImage (x,y) ->
+            let pixels = window9x9 (x,y)
+            let algorithmIndex =
+                pixels
+                |> Seq.map (getPixelValueOrZero state.Image state.InfinitePixelValue)
+                |> getDecimal
+            let newValue = Array.get state.Algorithm algorithmIndex
+            Map.add (x,y) newValue updatedImage
+        ) Map.empty toVisit
+    { state with
+        Image = newImage
+        InfinitePixelValue = nextInfinitePixelValue state.InfinitePixelValue
+    }
+
+
+let enhanceNTimes state times =
+    Seq.fold (fun s _ -> enhance s) state [1..times]
 
 let algorithm = input |> Seq.item 0 |> Seq.map charToInt |> Array.ofSeq
 
@@ -71,8 +85,12 @@ let image =
         for x in 0..size-1 -> ((x,y), img[y][x])]
     |> Map.ofList
 
-let iimage = enhance algorithm image
+let state = {
+    Image = image
+    Algorithm = algorithm
+    InfinitePixelValue = 0
+}
 
-printImage image
-printImage iimage
-// window9x9 (2,2) |> Seq.map (getPixelValueOrZero image) |> getDecimal
+let result = enhanceNTimes state 2
+
+result.Image.Values |> Seq.filter (fun v -> v = 1) |> Seq.length
