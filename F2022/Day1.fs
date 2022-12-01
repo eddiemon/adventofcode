d89f608177a6eecf54b1d7d645312d51f2c2b197
Eddie Sundvall Säther <eddie.sundvall@securitas.com>
2022-12-01 06:34:38 +0100

    Add 2022 day 1

---
+++ F2022/Day1.fs
@@ -0,0 +1,32 @@
+module Day1
+
+open System
+open Xunit
+open Xunit.Abstractions
+
+let text = System.IO.File.ReadAllText("../../../day1.txt")
+
+let caloriesPerElf =
+    text.Split("\n\n")
+    |> Seq.map (fun s -> s.Split("\n") |> Seq.map int)
+
+type Tests(output: ITestOutputHelper) =
+    [<Fact>]
+    let ``Part 1`` () =
+        let max =
+            caloriesPerElf
+            |> Seq.map (fun calories -> Seq.sum calories)
+            |> Seq.max
+            
+        output.WriteLine(string max)
+
+    [<Fact>]
+    let ``Part 2`` () =
+        let max3 =
+            caloriesPerElf
+            |> Seq.map (fun calories -> Seq.sum calories)
+            |> Seq.sortDescending
+            |> Seq.take 3
+            |> Seq.sum
+            
+        output.WriteLine(string max3)
