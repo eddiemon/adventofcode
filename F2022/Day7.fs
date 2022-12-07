d8c02b7a0d3be6e2fc6857055bb8a407f9112035
Eddie Sundvall Säther <eddie.sundvall@securitas.com>
2022-12-07 14:23:57 +0100

    Refactor 2022 day 7

--- F2022/Day7.fs
+++ F2022/Day7.fs
@@ -12,19 +12,26 @@ type Directory =
     { Name: string
       mutable Dirs: Directory list
       mutable Files: File list }
+    
     member this.Size() =
+        lazy (
             this.Files
             |> List.sumBy (fun f -> f.Size)
+        )
 
     member this.TotalSize() =
-        this.Size() + (this.Dirs |> List.sumBy (fun d -> d.TotalSize()))
-
+        lazy (
+            this.Size().Value + (this.Dirs |> List.sumBy (fun d -> d.TotalSize().Value))
+        )
 
 let createDirectory name = {Name = name; Dirs=List.empty;Files=List.empty }
 
 let rootDirectory = createDirectory "/"
 
 type Tests(output: ITestOutputHelper) =
+    let write obj =
+        output.WriteLine (string obj)
+
     let parseLine (dirStack: Directory list) (line: string) =
         let split = line.Split(" ")
         match split[0] with
@@ -44,53 +51,69 @@ type Tests(output: ITestOutputHelper) =
             let [|size;name|] = split
             dirStack.Head.Files <- {Name=name;Size=(int size)} :: dirStack.Head.Files
             dirStack
+            
+    let example =
+        """$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k"""
 
-    let input =
-//         """$ cd /
-// $ ls
-// dir a
-// 14848514 b.txt
-// 8504156 c.dat
-// dir d
-// $ cd a
-// $ ls
-// dir e
-// 29116 f
-// 2557 g
-// 62596 h.lst
-// $ cd e
-// $ ls
-// 584 i
-// $ cd ..
-// $ cd ..
-// $ cd d
-// $ ls
-// 4060174 j
-// 8033020 d.log
-// 5626152 d.ext
-// 7214296 k"""
-        System.IO.File.ReadAllText("../../../7.txt")
+    let personalInput = System.IO.File.ReadAllText("../../../7.txt")
+
+    let parseInput (inputType: string) =
+        let input =
+            match inputType with
+            | "example" -> example
+            | "input" -> personalInput
+        
+        input
             .Split('\n', StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries)
         |> Array.fold parseLine []
         |> List.last
         
-    [<Fact>]
-    let ``Part 1`` () =
+    [<Theory>]
+    [<InlineData("example")>]
+    [<InlineData("input")>]
+    let ``Part 1`` (inputType) =
         let rec SumSizeOver100000 (directory: Directory) =
             let dirSize = directory.TotalSize()
-            let size = if dirSize <= 100_000 then dirSize else 0
+            let size = if dirSize.Value <= 100_000 then dirSize.Value else 0
 
             let subdirSize =
                 directory.Dirs
                 |> List.sumBy (fun d -> SumSizeOver100000 d)
             
             size + subdirSize
-            
-        output.WriteLine (string (SumSizeOver100000 input))
-    
-    [<Fact>]
-    let ``Part 2`` () =
-        let freeSize = 70000000 - input.TotalSize()
+        
+        let rootDir = parseInput inputType
+        write (SumSizeOver100000 rootDir)
+
+    [<Theory>]
+    [<InlineData("example")>]
+    [<InlineData("input")>]
+    let ``Part 2`` (inputType) =
+        let rootDir = parseInput inputType
+        
+        let freeSize = 70000000 - rootDir.TotalSize().Value
         let spaceToFree = 30000000 - freeSize
         
         let rec flattenDirectories directory =
@@ -99,12 +122,12 @@ type Tests(output: ITestOutputHelper) =
                 |> List.collect flattenDirectories
             directory :: subDirs
 
-        let directories = flattenDirectories input
+        let directories = flattenDirectories rootDir
         let result =
             directories
-            |> List.filter (fun d -> d.TotalSize() >= spaceToFree)
-            |> List.sortBy (fun d -> d.TotalSize())
+            |> List.filter (fun d -> d.TotalSize().Value >= spaceToFree)
+            |> List.sortBy (fun d -> d.TotalSize().Value)
             |> List.head
-            |> fun d -> d.TotalSize()
+            |> fun d -> d.TotalSize().Value
         
         output.WriteLine (string result)
